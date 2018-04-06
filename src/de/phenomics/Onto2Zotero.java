43c5bdd792025258663cc4aa2441192fca1a5b83
koehlers <koehlers@10.31.38.11>
2018-04-06 18:02:00 +0200

    init

---
+++ src/de/phenomics/Onto2Zotero.java
@@ -0,0 +1,118 @@
+/**
+ * 
+ */
+package de.phenomics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import ontologizer.go.Ontology;
+import ontologizer.go.Term;
+import util.OntologyUtil;
+
+/**
+ * @author Sebastian Köhler (dr.sebastian.koehler@gmail.com)
+ *
+ */
+public class Onto2Zotero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// init json document
+		JSONArray arr = new JSONArray();
+		// get the obo files form cmd-line
+		int c = 0;
+		for (String oboFile : args) {
+			System.out.println("parse ontology: " + oboFile);
+			Ontology ontology = OntologyUtil.parseOntology(oboFile);
+			for (Term t : ontology) {
+				JSONObject termAsZotero = new JSONObject();
+				termAsZotero.put("id", t.getIDAsString());
+				termAsZotero.put("type", "article-journal");
+				termAsZotero.put("title", t.getIDAsString() + " : " + t.getName());
+				termAsZotero.put("container-title", t.getIDAsString());
+				JSONArray authorArray = new JSONArray();
+				boolean hasDefOrSyn = false;
+
+				if (t.getSynonyms() != null && t.getSynonyms().length > 0) {
+					hasDefOrSyn = true;
+
+					HashSet<String> allSyns = new HashSet<>();
+					for (String syn : t.getSynonyms()) {
+						allSyns.add(syn.toLowerCase());
+					}
+					allSyns = removeContained(allSyns);
+
+					for (String syn : allSyns) {
+						JSONObject authorObj = new JSONObject();
+						authorObj.put("family", syn);
+						authorArray.add(authorObj);
+					}
+				}
+				if (t.getDefinition() != null) {
+					hasDefOrSyn = true;
+					JSONObject authorObj = new JSONObject();
+					authorObj.put("family", t.getDefinition());
+					authorArray.add(authorObj);
+				}
+
+				// put at least one author
+				if (!hasDefOrSyn) {
+					JSONObject authorObj = new JSONObject();
+					authorObj.put("family", t.getName());
+					authorArray.add(authorObj);
+				}
+
+				termAsZotero.put("author", authorArray);
+				arr.add(termAsZotero);
+			}
+
+		}
+
+		String json = arr.toJSONString();
+		String jsonWithNl = json.replaceAll(",", ",\n");
+		try {
+
+			BufferedWriter out = new BufferedWriter(new FileWriter("phenotypes1.json"));
+			out.write(json);
+			out.close();
+
+			BufferedWriter out2 = new BufferedWriter(new FileWriter("phenotypes2.json"));
+			out2.write(jsonWithNl);
+			out2.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("done");
+
+	}
+
+	/**
+	 * @param allSyns
+	 * @return
+	 */
+	private static HashSet<String> removeContained(HashSet<String> allSyns) {
+		HashSet<String> remove = new HashSet<>();
+		for (String s : allSyns) {
+			for (String os : allSyns) {
+				if (os.length() > s.length())
+					if (os.contains(s))
+						remove.add(s);
+			}
+		}
+		if (remove.size() > 0) {
+			allSyns.removeAll(remove);
+		}
+		return allSyns;
+	}
+
+}
